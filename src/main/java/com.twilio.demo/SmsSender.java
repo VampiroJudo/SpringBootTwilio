2cad47a3d032872855641c5abf19d73f1c6bf16a
John Conley <johnconley507@hotmail.com>
2021-02-10 10:14:52 -0500

    Implementing twilio (personal account) in SpringBoot/Java

---
+++ src/main/java/com.twilio.demo/SmsSender.java
@@ -0,0 +1,6 @@
+package com.twilio.demo;
+
+public interface SmsSender {
+
+    void sendSms(SmsRequest smsRequest);
+}
---
+++ src/test/java/com/jringo/twilio/TwilioTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jringo.twilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
